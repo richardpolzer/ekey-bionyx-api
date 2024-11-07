0138b7229f0f667ca7d5cf98991c5c017c7152f3
Richard Polzer <richard@polzer-eb.de>
2024-11-07 10:25:14 +0100

    organized imports

--- ekey_bionyxpy/__init__.py
+++ ekey_bionyxpy/__init__.py
@@ -1,14 +1,14 @@
 """Provides an Interface with the ekey bionyx REST API."""
 
+from .auth import AbstractAuth
+from .models import BionyxAPI
 from .models import FunctionQuotas
+from .models import System
 from .models import SystemResponse
+from .models import Webhook
 from .models import WebhookData
 from .models import WebhookRename
 from .models import WebhookResponse
-from .auth import AbstractAuth
-from .models import BionyxAPI
-from .models import System
-from .models import Webhook
 
 
 __all__ = [
